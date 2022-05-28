9074f3f682837a0bb3b5cffec22d588b4738de33
Phongtor21 <pn02102001@gmail.com>
2022-05-28 21:59:39 +0700

    update footer

--- project-wow-ai/src/components/Layouts/Footer.js
+++ project-wow-ai/src/components/Layouts/Footer.js
@@ -1,27 +1,40 @@
 import { CardMedia, Container, Typography } from '@mui/material'
 
 import React from 'react'
+import { Col, Row } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <div style={{ backgroundColor: "var(--primary-bg)" }}>
-      <Container>
-        <div>
-          <Link to="/">
-            <CardMedia
-              component="img"
-              height="100%"
-              image='/Frame.png'
-            />
-          </Link>
-          <Typography style={{textAlign: "justify", color: "var(--primary-text)"}}>
-          A trusted partner that provides high-quality data solutions for AI training at a large scale.
-          </Typography>
-        </div>
-        <div>
-          Company
-        </div>
+    <div style={{ backgroundColor: "var(--primary-bg)", color: "var(--primary-text)" }}>
+      <hr />
+      <Container style={{width: "1330px"}}>
+        <Row>
+          <Col>
+            <Link to="/">
+              <CardMedia
+                component="img"
+                height="100%"
+                image='/Frame.png'
+              />
+            </Link>
+            <Typography style={{ textAlign: "justify" }}>
+              A trusted partner that provides high-quality data solutions for AI training at a large scale.
+            </Typography>
+          </Col>
+          <Col>
+            <Typography>Company</Typography>
+          </Col>
+          <Col>
+            <Typography>Data Collection</Typography>
+          </Col>
+          <Col>
+            <Typography>Data Annotation</Typography>
+          </Col>
+          <Col>
+            <Typography>Data Transcription</Typography>
+          </Col>
+        </Row>
       </Container>
     </div>
   )
